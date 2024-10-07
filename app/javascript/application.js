09454a0f4b3e46c23005ecb6e5b9a03b58bc2ce8
jaden39 <jaden.chan54@gmail.com>
2024-10-07 00:20:53 -0700

    change recipe page to swipe

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,33 +1,29 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-// application.js
-//= require swiper
+import "@hotwired/turbo-rails";
+import "controllers";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const swiper = new Swiper('.swiper-container', {
-      direction: 'horizontal',
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      swipeHandler: true,
-      spaceBetween: 50,
-      on: {
-        slideChangeTransitionEnd: function () {
-          // Example: Capture swipe left or right
-          const direction = swiper.activeIndex > swiper.previousIndex ? 'right' : 'left';
-          
-          // Perform actions based on swipe direction
-          if (direction === 'right') {
-            console.log("Liked recipe");
-          } else {
-            console.log("Disliked recipe");
-          }
+  const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    swipeHandler: true,
+    spaceBetween: 50,
+    on: {
+      slideChangeTransitionEnd: function () {
+        // Example: Capture swipe left or right
+        const direction = swiper.activeIndex > swiper.previousIndex ? 'right' : 'left';
+        
+        // Perform actions based on swipe direction
+        if (direction === 'right') {
+          console.log("Liked recipe");
+        } else {
+          console.log("Disliked recipe");
         }
       }
-    });
+    }
   });
-  
+});
