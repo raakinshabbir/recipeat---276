3fe28968ca59787f786190600bcb1d316de4485f
Course Test <JAVIgit2@cs.sfu.ca>
2024-10-09 14:57:35 -0700

    rename addrecipe

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,4 +4,25 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def index
+    @recipe = Recipe.all
+    render json: @recipe
+  end
+
+  def create
+    @Recipe = Recipe.new(recipe_params)
+    @recipe.photo.attach(params[:photo]) if params[:photo]
+
+    if @recipe.save
+      render json: @recipe, status: :created
+    else
+      render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :instructions, :photo)
+  end
 end
