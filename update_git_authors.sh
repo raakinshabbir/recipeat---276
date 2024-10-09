babdc26bec7461b11e96f8d0e254ea3b2e481d1a
jaden39 <jaden.chan54@gmail.com>
2024-10-09 01:29:13 -0700

    log

---
+++ update_git_authors.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="jaden@Jadens-MacBook-Pro-7.local"
+CORRECT_NAME="jaden39"
+CORRECT_EMAIL="jaden.chan54@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
