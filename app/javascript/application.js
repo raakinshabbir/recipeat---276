a510b080adde9835ccd121adcc0328e838aadef8
jaden39 <jaden.chan54@gmail.com>
2024-10-09 14:14:04 -0700

    trying to fix css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
 import "controllers";
 
 //swiper js
@@ -19,11 +18,6 @@ document.addEventListener("DOMContentLoaded", function () {
       if (swiper.activeIndex !== lastIndex) {
         const activeSlide = swiper.slides[swiper.activeIndex];
         const previousIndex = swiper.previousIndex;
-        const recipeId = activeSlide.getAttribute('data-recipe-id'); // Get the recipe ID from data attribute
-        if (!recipeId) {
-          console.error("Recipe ID not found on the active slide");
-          return;
-        }
   
         //variables for overlay
         const likeOverlay = activeSlide.querySelector('.feedback-overlay.like');
@@ -32,25 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (direction === 'right') {
           console.log("Liked recipe");
           likeOverlay.style.display = 'flex';
-
-          fetch('/liked', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content') // Include CSRF token
-            },
-            body: JSON.stringify({
-              recipe_id: recipeId
-            })
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log("Recipe liked successfully:", data);
-          })
-          .catch(error => {
-            console.error("Error liking recipe:", error);
-          });
-
           setTimeout(() => {
             likeOverlay.style.display = 'none';
           }, 500); // Hide after 1 second
@@ -70,5 +45,4 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
-
 
