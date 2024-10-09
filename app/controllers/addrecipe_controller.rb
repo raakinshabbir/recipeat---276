4bc3dffa1ed60271bc8313fc29fa577caf51142e
Course Test <JAVIgit2@cs.sfu.ca>
2024-10-09 16:41:56 -0700

    updated add controller

--- app/controllers/addrecipe_controller.rb
+++ app/controllers/addrecipe_controller.rb
@@ -1,16 +1,18 @@
 class AddrecipeController < ApplicationController
-    def index
-      @recipes = Recipe.all
-      render json: @recipes
-    end
+  def index
+    @recipes = Recipe.all
+    # Render the HTML view instead of JSON
+    # This will look for app/views/addrecipe/index.html.erb
+    render :index
+  end
 
-    def create
-      recipe = Recipe.new(recipe_params)
-      if recipe.save
-        render json: recipe
-      else
-        render json: recipe.errors
-      end
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   private
