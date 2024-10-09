36b42f3b7a9f437b01b7c9c1781e5b99a04bd349
Course Test <JAVIgit2@cs.sfu.ca>
2024-10-09 14:19:40 -0700

    updated recipe controller

--- app/controllers/addrecipe_controller.rb
+++ app/controllers/addrecipe_controller.rb
@@ -1,4 +1,21 @@
 class AddrecipeController < ApplicationController
     def index
+      @recipes = Recipe.all
+      render json: @recipes
     end
+
+    def create
+      recipe = Recipe.new(recipe_params)
+      if recipe.save
+        render json: recipe
+      else
+        render json: recipe.errors
+      end
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :origin, :instructions, :image)
+  end
+end
