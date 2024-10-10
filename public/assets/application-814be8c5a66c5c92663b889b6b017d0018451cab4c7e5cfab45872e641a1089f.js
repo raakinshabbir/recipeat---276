b5ac043ceb54a8e67436b881bd64c1fcd9219205
jaden39 <jaden.chan54@gmail.com>
2024-10-09 18:19:54 -0700

    added new liked

---
+++ public/assets/application-814be8c5a66c5c92663b889b6b017d0018451cab4c7e5cfab45872e641a1089f.js
@@ -0,0 +1,49 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails";
+import "controllers";
+
+//swiper js
+document.addEventListener("turbo:load", function () {
+
+
+  const swiperContainer = document.querySelector('swiper-container');
+  
+  if (swiperContainer) {
+    let lastIndex = swiperContainer.swiper.activeIndex
+
+    swiperContainer.addEventListener('swiperslidechange', (event) =>{
+      
+      const swiper = swiperContainer.swiper
+      const direction = swiper.activeIndex > swiper.previousIndex ? 'right' : 'left';
+      
+      if (swiper.activeIndex !== lastIndex) {
+        const activeSlide = swiper.slides[swiper.activeIndex];
+        const previousIndex = swiper.previousIndex;
+  
+        //variables for overlay
+        const likeOverlay = activeSlide.querySelector('.feedback-overlay.like');
+        const dislikeOverlay = activeSlide.querySelector('.feedback-overlay.dislike');
+        
+        if (direction === 'right') {
+          console.log("Liked recipe");
+          likeOverlay.style.display = 'flex';
+          setTimeout(() => {
+            likeOverlay.style.display = 'none';
+          }, 500); // Hide after 1 second
+        } else if (direction === 'left') {
+          console.log("Disliked recipe");
+          dislikeOverlay.style.display = 'flex';
+          setTimeout(() => {
+            dislikeOverlay.style.display = 'none';
+          }, 500); // Hide after 1 second
+        }
+
+        lastIndex = swiper.activeIndex
+      }
+    });
+  } else {
+    console.log("Swiper container not found");
+  }
+
+});
+
---
+++ test/models/likedrecipe_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LikedrecipeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
