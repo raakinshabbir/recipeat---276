4f75b9329357b298c4ec549de84fcce9dbb4e7b9
Asfand Khan <akhan90584@gmail.com>
2024-10-09 20:22:53 -0700

    Completed filtering recipes page. Integrated filtering to swiping interface.

--- db/migrate/20241009225700_create_likedrecipes.rb
+++ db/migrate/20241009225700_create_likedrecipes.rb
@@ -1,4 +1,4 @@
-class CreateLikedRecipes < ActiveRecord::Migration[7.2]
+class CreateLikedrecipes < ActiveRecord::Migration[7.2]
   def change
     create_table :liked_recipes do |t|
       t.references :user, null: false, foreign_key: true
