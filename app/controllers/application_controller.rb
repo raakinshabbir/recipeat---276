f7cb20f8bc26bfc064bd862f887bbcda2046ebff
Raakin Shabbir <raakin.shabbir04@gmail.com>
2024-10-09 21:31:35 -0700

    fixed routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,22 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   protected
 
-  def after_sign_in_path_for(resource)
-    swipe_path # Or any other path
-  end
-
-  # Filtering logic for recipes
-  private
   def filter_recipes(recipes)
-  
-    # Filter off difficulty
-    recipes = recipes.where(difficulty: params[:difficulty]) if params[:difficulty].present?
-    
-    # Filter off cooking time
-    recipes = recipes.where('cooking_time <= ?', params[:cooking_time]) if params[:cooking_time].present?
+    if params[:difficulty].present?
+      recipes = recipes.where(difficulty: params[:difficulty])
+    end
 
-    # Filter out recipes with unwanted ingredients
+    if params[:cooking_time].present?
+      recipes = recipes.where('cooking_time <= ?', params[:cooking_time])
+    end
+
     if params[:unwanted_ingredients].present?
-      unwanted_ingredients = params[:unwanted_ingredients].split(',').map(&:strip).map(&:downcase)
-
-      recipes = recipes.reject do |recipe|  
-        recipe_ingredients = recipe.ingredients.downcase.split(',').map(&:strip)
-        unwanted_ingredients.any? { |ingredient| recipe_ingredients.include?(ingredient) }
+      unwanted_ingredients = params[:unwanted_ingredients].split(',').map(&:strip)
+      unwanted_ingredients.each do |ingredient|
+        recipes = recipes.where.not('ingredients LIKE ?', "%#{ingredient}%")
       end
-    end 
+    end
+
     recipes
   end
 end
