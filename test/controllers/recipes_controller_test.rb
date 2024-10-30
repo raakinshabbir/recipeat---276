5633de2ee2db22804456bc5f51d3d769efab642f
JaviGIT2 <jcs28@sfu.ca>
2024-10-30 14:53:07 -0700

    rotd now holds for 24h

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,19 @@
-require "test_helper"
+require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @recipe1 = Recipe.create(title: 'Pasta', ingredients: 'Noodles, Sauce', instructions: 'Boil noodles', cooking_time: 20)
+    @recipe2 = Recipe.create(title: 'Salad', ingredients: 'Lettuce, Dressing', instructions: 'Mix ingredients', cooking_time: 5)
+  end
+
+  test 'should get recipe_of_the_day' do
+    get recipe_of_the_day_path, as: :json
+    assert_response :success
+    assert_includes [@recipe1, @recipe2], assigns(:recipe_of_the_day)
+  end
+
+  test 'should render template for HTML' do
+    get recipe_of_the_day_path
+    assert_template :recipe_of_the_day
+  end
 end
