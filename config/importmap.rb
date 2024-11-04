822a06a5824da08ddc20a294970f52065bd581ed
Abrar Rahman <abrarr@cs.sfu.ca>
2024-11-03 20:40:51 -0800

    added the ability to unlike recipes

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "swiper", to: "https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"
+pin "liked", to: "liked.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
